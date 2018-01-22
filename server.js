6cb7ca96aa1439bd99c8d44127d056a1e580fbfb
Soumya Himanish Mohapatra <shmiitian@gmail.com>
2018-01-22 19:05:38 +0530

    server linked to dashboard

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-// const config = require('./config.JSON');
+const server = require('./config.json').server;
+const routes = {
+  dashboard: require('./routes/dashboard').route
+};
+
 
 app.use('/',express.static(path.join(__dirname,'frontend')));
+app.use('/dashboard',routes.dashboard);
 
-/*app.listen(config.SERVERPORT, () => {
-  console.log('server is running at http://localhost:' + config.SERVERPORT)
-});*/
+app.listen(server.SERVERPORT, () => {
+  console.log('server is running at http://localhost:' + server.SERVERPORT)
+});
 
-app.listen(3000, () => {
-  console.log('server is running at http://localhost:3000')});
