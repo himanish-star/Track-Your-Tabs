4560c4643e6d3a5eb972a32749795d55ddc1c14e
Soumya Himanish Mohapatra <shmiitian@gmail.com>
2018-01-22 17:59:19 +0530

    dashboard setup started

---
+++ frontend/scripts/dashboard.js
@@ -0,0 +1,21 @@
+
+window.onload = function () {
+  let followersButton = document.getElementById('followers'),
+    followingButton = document.getElementById('following'),
+    followersList = document.getElementById('followersList'),
+    followingList = document.getElementById('followingList');
+
+  followingList.style.display = 'none';
+
+
+  followersButton.onclick = function () {
+    followingList.style.display = 'none';
+    followersList.style.display = 'inherit';
+  };
+
+  followingButton.onclick = function () {
+    followersList.style.display = 'none';
+    followingList.style.display = 'inherit';
+  };
+
+};
