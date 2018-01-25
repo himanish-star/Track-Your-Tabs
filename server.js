fc2827af486292309c4d1ae0385bd679e92f3da9
shikhar-scs <shikhar9a@gmail.com>
2018-01-25 19:19:05 +0530

    changes to server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const server = require('./config.json').server;
+
 const routes = {
   dashboard: require('./routes/dashboard').route
 };
