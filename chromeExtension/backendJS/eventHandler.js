5ff0a05839bb69218bd2c2f95e23dfbe763fa363
shikhar-scs <shikhar9a@gmail.com>
2018-01-26 22:10:41 +0530

    organised code

--- chromeExtension/backendJS/eventHandler.js
+++ chromeExtension/backendJS/eventHandler.js
@@ -4,17 +4,17 @@ chrome.runtime.onMessage.addListener(function (message) {
   switch(messageAction) {
     case 'open home page':
       chrome.tabs.create({
-        url: "./frontend/index.html"
+        url: "../frontend/index.html"
       });
       break;
     case 'open dashboard':
       chrome.tabs.query({'active': true}, function(tabs) {
-        chrome.tabs.update(tabs[0].id, {url: "./frontend/templates/dashboard.html"});
+        chrome.tabs.update(tabs[0].id, {url: "../frontend/templates/dashboard.html"});
       });
       break;
     case 'open profile':
       chrome.tabs.query({'active': true}, function(tabs) {
-        chrome.tabs.update(tabs[0].id, {url: "./frontend/templates/404Error.html"});
+        chrome.tabs.update(tabs[0].id, {url: "../frontend/templates/404Error.html"});
       });
       break;
   }
--- chromeExtension/frontend/index.js
+++ chromeExtension/frontend/index.js
@@ -35,15 +35,16 @@ $(document).ready(
      }
 	    let newTab = $(`<div class="w3-container w3-card w3-white w3-round w3-margin"><br>
 											  <img src="${tabsData[tab].favIconUrl}" alt="Avatar" class="dashboardNewsAvatar w3-left w3-circle w3-margin-right">
-											  <h4>${tabsData[tab].title}</h4><br>
-											  <hr class="w3-clear">
+											  <a href ="${tabsData[tab].url}" target="_blank"><h4>${tabsData[tab].title}</h4></a>
+											  <br>
+											 <!--<hr class="w3-clear">
 											  <p>
 											  Total time spent in this week : {{time}}
 									      <br>
 										     Number of times opened in a day : {{times}}
 									      </p>
 									      <button type="button" class="w3-button w3-theme-d1 w3-margin-bottom"><i class="fa fa-area-chart"></i>  View analysis</button>
-									      <button type="button" class="w3-button w3-theme-d2 w3-margin-bottom"><i class="fa fa-bell"></i>  Check your surf limits</button>
+									      <button type="button" class="w3-button w3-theme-d2 w3-margin-bottom"><i class="fa fa-bell"></i>  Check your surf limits</button>-->
 									   </div>`);
 	    $('#tabsList').append(newTab);
     }
