2375a681d570af6f2b4d75a7cb547ea565f9e856
Soumya Himanish Mohapatra <shmiitian@gmail.com>
2018-01-28 20:11:25 +0530

    template added, and screenshot mech added

--- chromeExtension/backend/tabs.js
+++ chromeExtension/backend/tabs.js
@@ -1,5 +1,7 @@
+/*
 chrome.tabs.onActivated.addListener(sendData);
 chrome.tabs.onRemoved.addListener(sendData);
+*/
 
 chrome.runtime.onMessage.addListener(function (message) {
 	let messageAction = message.action;
@@ -15,7 +17,33 @@ function sendData() {
 	chrome.windows.getAll({"populate": true}, getWindows);
 
   function getWindows(windows) {
-    let listOfTabs = [];
+		let listOfTabs = [];
+  	function kolp(i) {
+  		if(i === windows[0].tabs.length){
+  			chrome.runtime.sendMessage({
+					action: 'tabs fetched',
+					tabs: listOfTabs
+				});
+  			return
+			}
+			chrome.tabs.update(windows[0].tabs[i].id, {active:true}, function () {
+				setTimeout(function () {
+					chrome.tabs.captureVisibleTab(windows[0].id, function (screenShotURL) {
+						let tabData = {
+							tab: windows[0].tabs[i],
+							url: screenShotURL
+						};
+						listOfTabs.push(tabData);
+						setTimeout(function () {
+							kolp(i+1);
+            }, 200)
+          })
+        }, 100)
+      });
+    }
+    kolp(0);
+
+    /*let listOfTabs = [];
     for(let window of windows) {
     	for(let tabs of window.tabs) {
     		listOfTabs.push(tabs);
@@ -24,6 +52,6 @@ function sendData() {
 		chrome.runtime.sendMessage({
 			action: 'tabs fetched',
 			tabs: listOfTabs
-		})
+		})*/
   }
 }
--- chromeExtension/frontend/scripts/dashboard.js
+++ chromeExtension/frontend/scripts/dashboard.js
@@ -1,6 +1,20 @@
 window.onload = function () {
-  chrome.runtime.sendMessage({
-    action: 'fetch tabs'
+  let backToTop = $('#back-to-top');
+  let fetchBtn = $('#fetchBtn');
+
+  if (backToTop.length) {
+    backToTop.on('click', function (e) {
+      e.preventDefault();
+      $('html,body').animate({
+        scrollTop: 0
+      }, 700);
+    });
+  }
+
+  fetchBtn.click(function () {
+    chrome.runtime.sendMessage({
+      action: 'fetch tabs'
+    });
   });
 
   chrome.runtime.onMessage.addListener(function (message) {
@@ -16,35 +30,64 @@ window.onload = function () {
   function displayInfo(tabsData) {
     let currentTabs = $('#currentTabs');
     currentTabs.html('');
-    for(let tab = 0; tab < tabsData.length; tab ++) {
-      if(tabsData[tab].title === '{{username}}' || tabsData[tab].title === 'Extensions') {
+    for(let i = 0; i < tabsData.length; i ++) {
+      if(tabsData[i].tab.title === 'dashboard' || tabsData[i].tab.title === 'Extensions') {
         continue;
       }
       let newTab = $(`
-        <div class="w3-container w3-card w3-white w3-round w3-margin">
-          <br>
-          <img src="${tabsData[tab].favIconUrl}" alt="Avatar" class="dashboardNewsAvatar w3-left w3-circle w3-margin-right">
-          <h4>${tabsData[tab].title}</h4><br>
+        <div class="col-md-6">
+          <div class="a">
+            <a href="#test-popup-1" class="open-popup-link">
+              <img src="${tabsData[i].url}" class="img-responsive" alt="Responsive image">
+              <div class="img-hover glass">
+                <div class="c-table">
+                  <div class="ct-cell">
+                    <h3 class="img-title">Glass</h3>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div id="test-popup-1" class="white-popup mfp-hide">
+            <div class="container-fluid">
+              <div class="row">
+                <div class="pop-up-color">
+                  <div class="col-md-8 p-l-0 p-r-0">
+                    <img src="../external%20assets/routes/images/1.jpg" class="img-responsive" alt="Responsive image">
+                  </div>
+                  <div class="col-md-4">
+                    <div>
+                      <h2 class="popup-head">Glass</h2>
+                    </div>
+                    <div>
+                      <p class="popup-parapraph">Etiam quis viverra lorem, in semper lorem. Sed nisl arcu euismod sit a amet nisi is a euismod sed cursus arcu elementum and ipsum arcu vivamus is quis venenatis orci and nullam dolore. Etiam quis viverra lorem, in semper lorem. Sed nisl arcu euismod still amet nisi aliqua.Eas Lorem ipsum dolor sit amet.</p>
+                    </div>
+                    <div class="pop-up-icon">
+                      <div class="row">
+                        <ul class="text-center popup-social-contact">
+                          <li class="popup-social-icons">
+                            <a href="#" class="icon pop-up fa-facebook"><span class="label">Facebook</span></a>
+                          </li>
+                          <li class="popup-social-icons">
+                            <a href="#" class="icon pop-up fa-twitter"><span class="label">Twitter</span></a>
+                          </li>
+                          <li class="popup-social-icons">
+                            <a href="#" class="icon pop-up fa-google-plus"><span class="label">GooglePlus</span></a>
+                          </li>
+                          <li class="popup-social-icons">
+                            <a href="#" class="icon pop-up fa-pinterest"><span class="label">pinterest</span></a>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       `);
       currentTabs.append(newTab);
     }
   }
-
-  let followersButton = document.getElementById('followers'),
-    followingButton = document.getElementById('following'),
-    followersList = document.getElementById('followersList'),
-    followingList = document.getElementById('followingList')
-
-  followingList.style.display = 'none'
-
-  followersButton.onclick = function () {
-    followingList.style.display = 'none'
-    followersList.style.display = 'inherit'
-  }
-
-  followingButton.onclick = function () {
-    followersList.style.display = 'none'
-    followingList.style.display = 'inherit'
-  }
-}
+};
