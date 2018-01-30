6c1297287cb6e9c047a73cb721088f43e47a1282
Soumya Himanish Mohapatra <shmiitian@gmail.com>
2018-01-30 08:14:20 +0530

    third form of history implemented, but some UI developments remain

--- chromeExtension/frontend/scripts/history.js
+++ chromeExtension/frontend/scripts/history.js
@@ -1,5 +1,42 @@
 window.onload = function () {
   let historyList = $('#historyList');
+  let visitList = $('#visitList');
+  let switchHistoryModes = $('#switchHistoryModes').children();
+  let viewParticularTop = $('#viewParticularTop');
+  let viewAllTop = $('#viewAllTop');
+  let viewAllBtn = $('#viewAllBtn');
+  let viewParticularBtn = $('#viewParticularBtn');
+  let getVisitsBtn = $('#getVisitsBtn');
+  let inputURL = $('#inputURL');
+
+  getVisitsBtn.click(function () {
+    chrome.runtime.sendMessage({
+      action: "fetch records of visit to a particular url",
+      url: inputURL.val()
+    });
+  });
+
+  switchHistoryModes.click(function (event) {
+    if (event.target.innerText === 'view all') {
+      event.target.classList.remove('btn-secondary');
+      event.target.classList.add('btn-primary');
+      viewParticularBtn.addClass('btn-secondary');
+      viewParticularBtn.removeClass('btn-primary');
+      viewParticularTop.hide();
+      viewAllTop.show();
+      visitList.hide();
+      historyList.show();
+    } else {
+      event.target.classList.remove('btn-secondary');
+      event.target.classList.add('btn-primary');
+      viewAllBtn.addClass('btn-secondary');
+      viewAllBtn.removeClass('btn-primary');
+      viewParticularTop.show();
+      viewAllTop.hide();
+      historyList.hide();
+      visitList.show();
+    }
+  });
 
   chrome.runtime.sendMessage({
     action: "fetch history of tabs opened in the past"
@@ -19,16 +56,43 @@ window.onload = function () {
   });
 
   function displayVisitsToURL(visits) {
-    historyList.inner("");
-    for(let visit of visits) {
-      let li = document.createElement('li');
-      li.innerText = visit.visitTime;
-      historyList.appendChild(li)
+    visitList.html(`
+      <thead>
+        <tr>
+          <th scope="col">Sr. No.</th>
+          <th scope="col">Time</th>
+        </tr>
+      </thead>
+      <tbody>
+      </tbody>
+    `);
+    for(let i=0; i<visits.length; i++) {
+      let time = visits[i].visitTime;
+      time = new Date(time);
+      let newRow = $(`
+        <tr>
+          <th scope="row">${visits.length-(i)}</th>
+          <td class="text-primary">${time}</td>
+        </tr>    
+      `);
+      visitList.prepend(newRow);
     }
   }
 
   function displayHistory(tabs) {
-    historyList.html("");
+    historyList.html(`
+      <thead>
+        <tr>
+          <th scope="col">Sr. No.</th>
+          <th scope="col">Title</th>
+          <th scope="col">Visits</th>
+          <th scope="col">Link</th>
+        </tr>
+      </thead>
+      <tbody>
+      </tbody>
+    `);
+
     for(let i=0; i<tabs.length; i++) {
       let newRow = $(`
         <tr>
