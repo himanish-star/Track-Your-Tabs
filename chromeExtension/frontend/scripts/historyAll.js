c6eb53e44f0d805c7dfc88f84cd6737915ef495b
Soumya Himanish Mohapatra <shmiitian@gmail.com>
2018-01-31 18:19:23 +0530

    fixed copy to clipboard and changed view single button color to purple

--- chromeExtension/frontend/scripts/historyAll.js
+++ chromeExtension/frontend/scripts/historyAll.js
@@ -1,7 +1,6 @@
 window.onload = function () {
   let historyList = $('#historyList');
-  // let copyButton =$('.copyToClipboard')
-	
+
   chrome.runtime.sendMessage({
     action: "fetch history of tabs opened in the past"
   });
@@ -32,39 +31,45 @@ window.onload = function () {
     `);
 
     for(let i=0; i<tabs.length; i++) {
-	    let newRow = $(`
+	    tabs[i].title = tabs[i].title.replace('<!--','');
+      let newRow = $(`
         <tr>
           <th scope="row">${i + 1}</th>
           <td class="text-primary" title="${tabs[i].title}">${tabs[i].title ? tabs[i].title.substring(0, 80) : 'no title'}</td>
           <td class="text-danger">${tabs[i].visitCount}</td>
-          <td><a href="${tabs[i].url}" target="_blank" class="visitLink">Visit Link</a></td>
-          <td><button class="btn btn-outline-primary copyToClipboard" id="copyToClip${i}" data-id="${tabs[i].url}"class="copyToClipboard">Copy To Clipboard</button></td>
+          <td><a href="${tabs[i].url}" target="_blank" class="visitLink"><i class="mdi mdi-link-variant"></i></a></td>
+          <td><button class="btn btn-outline-primary copyToClipboard" id="copyToClip${i}" data-id="${tabs[i].url}" class="copyToClipboard"><i class="mdi mdi-content-copy"></i></button></td>
         </tr>
       `);
+      newRow.find('.copyToClipboard').click(function (event) {
+        let link = event.target.getAttribute('data-id');
+        let $temp = $("<input>");
+        $("body").append($temp);
+        $temp.val(link).select();
+        document.execCommand("copy");
+        $temp.remove();
+      });
 	    historyList.append(newRow);
     }
-    setClipBoardData(tabs.length)
   }
-  
-  function setClipBoardData(size) {
 
+  /*function setClipBoardData(size) {
     for(let i = 0 ; i < size ;i++) {
-	     let el = document.getElementById('copyToClip' + i);
+      let el = document.getElementById('copyToClip' + i);
+      el.onclick = function () {
+        console.log('sda');
+        document.execCommand('copy')
+      }
 
-	    el.onclick = function () {
-		    document.execCommand('copy')
-	    }
-	
-	    el.addEventListener('copy', function (event) {
-		    event.preventDefault();
-		
-		    if (event.clipboardData) {
-			    console.log(event.target.getAttribute('data-id'))
-			    event.clipboardData.setData('text/plain', event.target.getAttribute('data-id'))
-		    }
-	    })
+      el.addEventListener('copy', function (event) {
+        event.preventDefault();
+
+        if (event.clipboardData) {
+          event.clipboardData.setData('text/plain', event.target.getAttribute('data-id'))
+        }
+      })
     }
-  }
+  }*/
   
 };
 
